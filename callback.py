720e0951d3f74fe57f536ccd3adee151389504b5
Benjie Jiao <benjiao12@gmail.com>
2015-08-10 07:22:01 +0800

    Added correct response in callback. Added handling for POST requests

--- callback.py
+++ callback.py
@@ -6,13 +6,14 @@ service should call.
 import logging
 from flask import Flask
 from flask import request
+from flask import make_response
 
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.DEBUG)
 app = Flask(__name__)
 
 
-@app.route('/<tag>/')
+@app.route('/<tag>/', methods=["GET"])
 def index(tag):
     logging.info("Tag: %s" % tag)
     logging.info("Request Data: %s" % request.data)
@@ -21,7 +22,15 @@ def index(tag):
     logging.info("Challenge: %s", challenge)
     if challenge is not None:
         return challenge
+    else:
+        return make_response("OK", 400)
 
-    return tag
+
+@app.route('/<tag>/', methods=["POST"])
+def receive(tag):
+    logging.info("Tag: %s" % tag)
+    logging.info("Request Data: %s" % request.data)
+
+    return make_response("OK", 200)
 
 app.run(host='0.0.0.0', debug=True)
