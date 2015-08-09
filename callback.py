09217af67313d8722c3b5bb52219224e500838ec
Benjie Jiao <benjiao12@gmail.com>
2015-08-10 06:47:47 +0800

    Added logging

--- callback.py
+++ callback.py
@@ -3,19 +3,22 @@ A simple flask app for handling callbacks that the instagram
 service should call.
 """
 
+import logging
 from flask import Flask
 from flask import request
 
+logging.basicConfig(format='%(levelname)s:%(message)s',
+                    level=logging.DEBUG)
 app = Flask(__name__)
 
 
 @app.route('/<tag>')
 def index(tag):
-    print "Tag: %s" % tag
-    print "Request Data: %s" % request.data
+    logging.info("Tag: %s" % tag)
+    logging.info("Request Data: %s" % request.data)
 
     challenge = request.args.get('hub.challenge')
-    print "Challenge: %s" % challenge
+    logging.info("Challenge: %s" % challenge)
     if challenge is not None:
         return challenge
 
